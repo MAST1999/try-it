b9bbbd8c09a8e8fe9dd4db540c265e6c2dc7fa7e
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2021-08-01 17:11:48 +0430

    exercises from part 1 to end of 4

---
+++ getting_inputs.py
@@ -0,0 +1,5 @@
+x = input("Enter your name: ")
+y = int(input("Enter your age: "))
+c = float(input("Enter your average "))
+
+print(f"I'm {x} and I am {y} years old, my average is : {c}")
---
+++ manipulating_strings_numbers.py
@@ -0,0 +1,7 @@
+x = 44
+y = "Hello there"
+c = 5j + 4
+
+res1 = x + c
+# res2 = x + y It doesn't work -> number + string or vise versa
+# res3 = y + c doesn't work for the same reasons
